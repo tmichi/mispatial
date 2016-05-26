025a5e1eac1a24b472372a5d9f0b09044c420565
MichikawaTakashi <michi@MichikawaTakashi-no-MacBook-Pro.local>
2016-05-26 22:50:57 +0900

    kdtree0.cpp changed

--- samples/kdtree0.cpp
+++ samples/kdtree0.cpp
@@ -1,27 +1,27 @@
 #include <mi/Kdtree.hpp>
-#include <mi/math.hpp>
+#include <Eigen/Dense>
 #include <vector>
 #include <iostream>
 int main ( int argc, char** argv )
 {
 
-        std::vector<mi::Vector3d> p;
+        std::vector<Eigen::Vector3d> p;
         for( int i = 0 ; i < 10000 ; i++ ) {
                 double x = ( rand() % 2001 - 1000 ) * 0.001;
                 double y = ( rand() % 2001 - 1000 ) * 0.001;
                 double z = ( rand() % 2001 - 1000 ) * 0.001;
-                p.push_back( mi::Vector3d( x,y,z ) );
+                p.push_back( Eigen::Vector3d( x,y,z ) );
         }
 
-        mi::Kdtree<mi::Vector3d> kdtree( p ) ;
+        mi::Kdtree<Eigen::Vector3d> kdtree( p ) ;
 
-        mi::Vector3d v( 0.1, 0.1, 0.1 ) ;
-        std::list<mi::Vector3d> result;
+        Eigen::Vector3d v( 0.1, 0.1, 0.1 ) ;
+        std::list<Eigen::Vector3d> result;
 
         kdtree.find( v, 0.1, result, true );
-        for( std::list<mi::Vector3d>::iterator iter = result.begin(); iter != result.end() ; ++iter ) {
-                mi::Vector3d& v0 = *iter;
-                mi::Vector3d d = *iter - v;
+        for( std::list<Eigen::Vector3d>::iterator iter = result.begin(); iter != result.end() ; ++iter ) {
+                Eigen::Vector3d& v0 = *iter;
+                Eigen::Vector3d d = *iter - v;
                 double sqrtDist = d.x()*d.x()+d.y()*d.y()+d.z()*d.z();
                 std::cerr<<v0.x()<<" "<<v0.y()<<" "<<v0.z()<<" "<<sqrtDist<<std::endl;
         }
